4de4d1220ae7de07c6f7f4938b433b89bf96b85a
Arthur Correnson <arthur.correnson@gmail.com>
2021-01-20 21:01:29 +0100

    Renaming CMake

--- src/vm.c
+++ src/vm.c
@@ -7,10 +7,6 @@ void fail(char *msg) {
   exit(1);
 }
 
-/**
- * @brief Initialize a new VM with empty stack
- *
- */
 VM *new_VM(unsigned int gc_thresh) {
   VM *vm = malloc(sizeof(VM));
   vm->stack_size = 0;
@@ -21,13 +17,6 @@ VM *new_VM(unsigned int gc_thresh) {
   return vm;
 }
 
-/**
- * @brief Allocate a new object on the heap
- *
- * @param vm
- * @param type
- * @return Object*
- */
 Object *new_object(VM *vm, t_object type) {
   if (vm->num_objects >= vm->max_objects && vm->gc_on)
     gc(vm);
@@ -46,49 +35,24 @@ void vm_gc_off(VM *vm) { vm->gc_on = false; }
 
 void vm_gc_on(VM *vm) { vm->gc_on = true; }
 
-/**
- * @brief Push a value on the stack
- *
- * @param vm
- * @param value
- */
 void push(VM *vm, Object *value) {
   if (vm->stack_size >= MAX_STACK_SIZE)
     fail("Stack overflow !");
   vm->stack[vm->stack_size++] = value;
 }
 
-/**
- * @brief Pop a value from the stack
- *
- * @param vm
- * @return Object*
- */
 Object *pop(VM *vm) {
   if (vm->stack_size <= 0)
     fail("Stack underflow !");
   return vm->stack[--vm->stack_size];
 }
 
-/**
- * @brief Push an integer on the stack
- *
- * @param vm
- * @param int_value
- */
 void push_int(VM *vm, int int_value) {
   Object *obj = new_object(vm, OBJ_INT);
   obj->value = int_value;
   push(vm, obj);
 }
 
-/**
- * @brief Pop 2 objects from the stack, make a pair out of them and push it back
- * to the stack
- *
- * @param vm
- * @return Object*
- */
 Object *push_pair(VM *vm) {
   Object *obj = new_object(vm, OBJ_PAIR);
   obj->right = pop(vm);
--- src/vm.h
+++ src/vm.h
@@ -41,22 +41,74 @@ typedef struct {
   Object *first_object;
 } VM;
 
+/**
+ * @brief Initialise a new VM
+ *
+ * @param gc_thresh Garbage Collector threshold
+ * @return VM*
+ */
 VM *new_VM(unsigned int gc_thresh);
 
+/**
+ * @brief Disable the GC
+ *
+ * @param vm
+ */
 void vm_gc_off(VM *vm);
 
+/**
+ * @brief Enable the GC
+ *
+ * @param vm
+ */
 void vm_gc_on(VM *vm);
 
+/**
+ * @brief Allocate a new object on the heap
+ *
+ * @param vm
+ * @param type
+ * @return Object*
+ */
 Object *new_object(VM *vm, t_object type);
 
+/**
+ * @brief Push an object on the stack
+ *
+ * @param vm
+ * @param value
+ */
 void push(VM *vm, Object *value);
 
+/**
+ * @brief Prop on object from the stack
+ *
+ * @param vm
+ * @return Object*
+ */
 Object *pop(VM *vm);
 
+/**
+ * @brief Allocate an integer and push it to the stack
+ *
+ * @param vm
+ * @param int_value
+ */
 void push_int(VM *vm, int int_value);
 
+/**
+ * @brief Pop 2 values from the stack, make a pair out of them and push it back
+ *
+ * @param vm
+ * @return Object*
+ */
 Object *push_pair(VM *vm);
 
+/**
+ * @brief Pop two values and push vector sum
+ *
+ * @param vm
+ */
 void vsum(VM *vm);
 
 void debug_object(Object *obj);
